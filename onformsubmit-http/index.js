a1d0f2c355189b6f78e96437fe70c00cb9c55ba7
Luan <ltran@asymmetrik.com>
2021-07-19 14:14:43 -0400

    feat: add auth/workspace creation

--- onformsubmit-http/index.js
+++ onformsubmit-http/index.js
@@ -1,7 +1,6 @@
 /**
  * Handles Google Form submission answers
  */
-const { Firestore } = require("@google-cloud/firestore");
 const google = require("googleapis");
 const axios = require("axios").default;
 
@@ -9,10 +8,24 @@ const axios = require("axios").default;
 // make changes as needed
 const SERVICE_ACCOUNT_KEY = "./creds.json";
 const FIRECLOUD_URL = "https://api.firecloud.org";
-const FIRESTORE_COLLECTION = "submission-answers";
+const ADMIN_EMAILS = [
+  "vreeves@broadinstitute.org",
+  "candace@broadinstitute.org",
+  "ltran@asymmetrik.com",
+];
 
 exports.onFormSubmit = async (req, res) => {
   const message = req.body;
+
+  // parse creds file
+  let serviceAccountEmail;
+  try {
+    const creds = require(SERVICE_ACCOUNT_KEY);
+    serviceAccountEmail = creds.client_email;
+  } catch (err) {
+    console.error(`[Error] Could not read creds file: ${err}`);
+    return res.status(500).send(`[500] Could not read creds file: {err}`);
+  }
 
   // extract data from submission message
   let submissionTime, email, editLink, questions, responses;
@@ -36,12 +49,21 @@ exports.onFormSubmit = async (req, res) => {
   for (let i = 0; i < questions.length; i++) {
     const qna = `${questions[i]}: ${responses[i]}`;
     questionAndAnswerList.push(qna);
-    console.log(qna);
+    console.info(qna);
   }
+
+  // TODO: fill this out some more
+  // extract neccessary data from submission message
+  const billingProject = "anvil-dev-fhir2";
+  const workspaceName = "hammer_test_space";
+  const groupName = "AUTH_Asym_Test";
+  const submitterEmail = email;
 
   // setup auth
   try {
     const bearerToken = await getOAuth2Token();
+    // TODO: remove this client
+    // console.log(bearerToken);
     axios.defaults.baseURL = FIRECLOUD_URL;
     axios.defaults.headers.common["Authorization"] = `Bearer ${bearerToken}`;
   } catch (err) {
@@ -49,42 +71,93 @@ exports.onFormSubmit = async (req, res) => {
     return res.status(401).send(`[401] Unauthorized: ${err}`);
   }
 
-  // TODO: create workspace
-  // POST /api/workspaces
+  // create and add submitter + anvil-admins to auth domain
+  // POST /api/groups/${groupName}
+  // PUT /api/groups/${groupName}/${role}/${email}
+  let groupEmail;
   try {
-    // create request body
-    const body = {
-      namespace: "billing_project",
-      name: "hammer_test_space",
+    // create new group
+    console.info(`[LOG] Creating group ${groupName}`);
+    await axios.post(`/api/groups/${groupName}`).then((res) => {
+      groupEmail = res.data.groupEmail;
+    });
+
+    // add admin emails
+    ADMIN_EMAILS.forEach(async (email) => {
+      console.info(`[LOG] Adding ${email} to group ${groupName}`);
+      await axios.put(`/api/groups/${groupName}/admin/${email}`);
+    });
+
+    // add submitter email
+    console.info(`[LOG] Adding ${submitterEmail} to group ${groupName}`);
+    await axios.put(`/api/groups/${groupName}/admin/${submitterEmail}`);
+
+    // add service account email
+    console.info(`[LOG] Adding ${serviceAccountEmail} to group ${groupName}`);
+    await axios.put(`/api/groups/${groupName}/admin/${serviceAccountEmail}`);
+  } catch (err) {
+    console.error(`[Error] /api/groups call: ${err}`);
+    return res.status(400).send(`[400] Bad Request: ${err}`);
+  }
+
+  // create and share workspace
+  // POST /api/workspaces
+  // PATCH /api/workspaces/${workspaceNamespace}/${workspaceName}/acl
+  try {
+    // create workspace request
+    const workspaceRequest = {
+      namespace: billingProject,
+      name: workspaceName,
       authorizationDomain: [
         {
-          membersGroupName: "AUTH_Asymmetrik",
+          membersGroupName: groupName,
         },
       ],
       attributes: {},
       noWorkspaceOwner: false,
     };
-
-    await axios.get("/api/workspaces").then((res) => {
-      console.log(res);
+    await axios.post("/api/workspaces", workspaceRequest).then((res) => {
+      console.log(res.data);
     });
 
-    // EX: how to make a POST request
-    // await axios.post("/api/workspaces", body).then((res) => {
-    //   console.log(res);
-    // });
+    // create share request
+    const aclRequest = [
+      {
+        email: groupEmail,
+        accessLevel: "OWNER",
+        canShare: true,
+        canCompute: true,
+      },
+    ];
+    await axios
+      .patch(
+        "/api/workspaces/anvil-dev-fhir2/hammer_test_space/acl",
+        aclRequest
+      )
+      .then((res) => {
+        console.log(res.data);
+      });
   } catch (err) {
-    console.error(`[Error] /api/workspaces call: ${err}`);
+    console.error(err);
+    console.error(`[Error] Failed to create workspace: ${err}`);
     return res.status(400).send(`[400] Bad Request: ${err}`);
   }
 
-  // TODO: add auth domain
-  // POST /api/groups/${groupName}
-
-  // TODO: add submitter + anvil-admins to auth domain
-  // PUT /api/groups/${groupName}/${role}/${email}
-
   // TODO: remove service account from workspace/auth domain
+  try {
+    // const credsFile = require(SERVICE_ACCOUNT_KEY);
+    // console.info(
+    //   `[LOG] Removing ${credsFile.client_email} from group ${groupName}`
+    // );
+    // await axios.delete(
+    //   `/api/groups/${groupName}/admin/${credsFile.client_email}`
+    // );
+  } catch (err) {
+    console.error(
+      `[Error] Failed to remove ${serviceAccountEmail} from workspace: ${err}`
+    );
+    return res.status(400).send(`[400] Bad Request: ${err}`);
+  }
 
   // TODO: Replace with meaningful message
   const msg = "Completed Run!";
@@ -104,6 +177,7 @@ const getOAuth2Token = async () => {
     // "https://www.googleapis.com/auth/cloud-billing",
   ];
 
+  // get access token
   try {
     const googleAuthClient = new google.Auth.GoogleAuth({
       keyFile: SERVICE_ACCOUNT_KEY,
@@ -115,35 +189,3 @@ const getOAuth2Token = async () => {
     throw new Error(err);
   }
 };
-
-/*
-// TODO: log data data into firestore
-//        this will require us to have created a workspace first
-//        to have a collectionName to log the data
-  try {
-    const firestore = new Firestore();
-    const collection = firestore.collection(FIRESTORE_COLLECTION);
-
-    // create document payload
-    const payload = {
-      submissionTime,
-      submitterEmail: email,
-      editLink,
-      questions,
-      responses,
-      questionAndAnswerList,
-    };
-
-    // create document
-    // TODO: replace with workspace name
-    const document = await collection
-      .doc("DEFAULT_WORKSPACE")
-      .set(message, { merge: true });
-    console.log(
-      `${document.writeTime.toDate()}: Document written to ${FIRESTORE_COLLECTION}`
-    );
-  } catch (err) {
-    console.error(`[Error] Firestore: ${err}`);
-    res.status(400).send(`[400] Bad Request: ${err}`);
-  }
-*/
