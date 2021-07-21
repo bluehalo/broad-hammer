5abd8ff5a31b2d718ca0d1017d1d9709049fb18c
Luan <ltran@asymmetrik.com>
2021-07-21 10:22:54 -0400

    feat: add auth lib

---
+++ onformsubmit-http/utils/auth.test.js
@@ -0,0 +1,26 @@
+const Auth = require("./auth");
+
+const CREDS_FILE = "../creds.json";
+
+describe("Sanity Test", () => {
+  it("should get OAuth2 token", async () => {
+    // create auth instance
+    const auth = new Auth(CREDS_FILE);
+    expect(auth.credsFile()).toEqual(CREDS_FILE);
+    expect(auth.token()).toBeUndefined();
+
+    // get token
+    await auth.requestAccessToken();
+    expect(auth.token()).toBeDefined();
+  });
+
+  it("should parse client_email from creds file", async () => {
+    // create auth instance
+    const auth = new Auth(CREDS_FILE);
+
+    // DEV: change this to creds file's client_email
+    expect(auth.clientEmail()).toBe(
+      "hammer@gcp-testing-308520.iam.gserviceaccount.com"
+    );
+  });
+});
