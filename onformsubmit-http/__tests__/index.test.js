532300b35ede337315af2722b6927657990e1a71
Luan <ltran@asymmetrik.com>
2021-08-25 14:12:39 -0400

    ci: update secrets config

--- onformsubmit-http/__tests__/index.test.js
+++ onformsubmit-http/__tests__/index.test.js
@@ -1,15 +1,9 @@
 describe("Environment Tests", () => {
   it("ensure env vars are set", async () => {
-    // DEV: this will need to be updated when setting up prod
-    expect(process.env.FIRECLOUD_URL).toEqual(
-      "https://firecloud-orchestration.dsde-dev.broadinstitute.org"
-    );
+    expect(process.env.FIRECLOUD_URL).toEqual("https://api.firecloud.org");
     expect(process.env.SERVICE_ACCOUNT_KEY).toEqual("creds.json");
-    expect(process.env.DEFAULT_BILLING_PROJECT).toEqual(
-      "general-dev-billing-account"
-    );
-    expect(process.env.ADMIN_EMAIL).toEqual(
-      "Auth_HAMMER_Testing@dev.test.firecloud.org"
-    );
+    // DEV: This will need to be changed to `anvil-datastorage` before prod
+    expect(process.env.DEFAULT_BILLING_PROJECT).toEqual("anvil-dev-fhir2");
+    expect(process.env.ADMIN_EMAIL).toEqual("anvil-admins@firecloud.org");
   });
 });
